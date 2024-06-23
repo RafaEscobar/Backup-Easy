ae581326855f8b13ff5397d1f3c55b9d1c145c70
Rafael Escobar Gutierrez <Rafael.EG.code@gmail.com>
2024-06-23 15:53:35 -0600

    Agrega relaciones a modelo Section

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Section extends Model
 {
     use HasFactory;
+
+    public function tag()
+    {
+        return $this->belongsTo(Tag::class);
+    }
+
+    public function sectionType()
+    {
+        return $this->belongsTo(SectionType::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function accesses()
+    {
+        return $this->hasMany(Access::class);
+    }
 }
